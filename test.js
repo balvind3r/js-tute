27ce8ccb91eabb9bd36373b966a6b90dede2a8f2
hekate <balvindersingh485280@gmail.com>
2023-12-29 21:40:35 +0530

    0010.js

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
 console.log("Hello");
 console.log("Hello");
+
+// Const can't be changed
+const accountId = 12344 
+// scope problem solved in let
+let accountEmail = "ABC@GMAIL.COM"
+// scope problem existed in var
+var accountPassword = "12345"
+// Not a good practice
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
