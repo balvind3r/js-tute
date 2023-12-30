c7c8c66355ddd6d574a80678b0171e7faf548552
hekate <balvindersingh485280@gmail.com>
2023-12-30 19:46:38 +0530

    objects 2

--- 02-Basics/objects_2.js
+++ 02-Basics/objects_2.js
@@ -30,9 +30,26 @@ const obj2 = {3: 'a', 4: 'b'}
 // const obj3 = {...obj1, ...obj2};
 // console.log(obj3)
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
 
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "Balvinder"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+console.log(Instructor)
+
+
+// {
+//     name: "Balvinder",
+//     coursename: "Js",
+//     price: "0"
+// }
